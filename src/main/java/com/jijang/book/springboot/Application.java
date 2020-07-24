d46dd9ed45193a280a775fcbe920417f29195ab9
장진익 <jijang@mz.co.kr>
2020-07-24 15:39:50 +0900

    2020. 07.24 개발 전, 테스트를 위한 기초 작업 - Controller 테스트 - Lombok 설치 - Dto 테스트

---
+++ src/main/java/com/jijang/book/springboot/Application.java
@@ -0,0 +1,13 @@
+package com.jijang.book.springboot;
+
+import javax.swing.Spring;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
