8a1793b0005d94b0125ab3eb931a3255e42c2302
장진익 <jijang@mz.co.kr>
2020-07-26 17:30:34 +0900

    2020. 07.26 JPA 사용하여 Entity CRU 작업 - JPA 라이브러리 설정 - PostsEntity 생성 - JPA 생성/수정/조회 처리 구현 및 테스트 - H2 라이브러리 추가 및 console, query log 설정

--- src/main/java/com/jijang/book/springboot/Application.java
+++ src/main/java/com/jijang/book/springboot/Application.java
@@ -3,7 +3,9 @@ package com.jijang.book.springboot;
 import javax.swing.Spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
   public static void main(String[] args) {
