61fbdf340cfa8ddc151bad01a1007e7a3aa9569f
장진익 <jijang@mz.co.kr>
2020-07-26 22:43:40 +0900

    2020. 07.26 게시판 CRUD 구현하기 - Mustache 사용하여 등록, 수정 템플릿 만들기 - API 콘트롤러와 View 콘트롤러 수정하기 - 등록, 수정 및 삭제 View 구성하기 - index.js 로 이벤트에 대한 처리하기

--- src/main/java/com/jijang/book/springboot/web/controller/PostsApiController.java
+++ src/main/java/com/jijang/book/springboot/web/controller/PostsApiController.java
@@ -5,6 +5,7 @@ import com.jijang.book.springboot.web.dto.PostsResponseDto;
 import com.jijang.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jijang.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,16 +21,26 @@ public class PostsApiController {
 
   @PostMapping("/api/v1/posts")
   public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+
     return postsService.save(requestDto);
   }
 
   @PutMapping("/api/v1/posts/{id}")
   public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+
     return postsService.update(id, requestDto);
   }
 
   @GetMapping("/api/v1/posts/{id}")
   public PostsResponseDto findById(@PathVariable Long id) {
+
     return postsService.findById(id);
   }
+
+  @DeleteMapping("/api/v1/posts/{id}")
+  public Long delete(@PathVariable Long id) {
+    postsService.delete(id);
+    return id;
+  }
+
 }
--- src/main/java/com/jijang/book/springboot/web/domain/posts/PostsService.java
+++ src/main/java/com/jijang/book/springboot/web/domain/posts/PostsService.java
@@ -1,11 +1,15 @@
 package com.jijang.book.springboot.web.domain.posts;
 
+import com.jijang.book.springboot.web.dto.PostsListResponseDto;
 import com.jijang.book.springboot.web.dto.PostsResponseDto;
 import com.jijang.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jijang.book.springboot.web.dto.PostsUpdateRequestDto;
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -34,4 +38,20 @@ public class PostsService {
 
     return new PostsResponseDto(entity);
   }
+
+  @Transactional(readOnly = true)
+  public List<PostsListResponseDto> findAllDesc() {
+    return postsRepository.findAllDesc().stream()
+        .map(PostsListResponseDto::new)
+        .collect(Collectors.toList());
+  }
+
+  @Transactional
+  public void delete(Long id) {
+    Posts posts = postsRepository.findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+    postsRepository.delete(posts);
+  }
+
 }
