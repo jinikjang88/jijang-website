e54f0219af0e230b9e7fba639494409c5fd49ba4
장진익 <jijang@mz.co.kr>
2020-07-28 09:17:17 +0900

    2020. 07.28 Google API 연동 전 작업 - User 클래스 생성 - Role enum 클래스 생성

---
+++ src/main/java/com/jijang/book/springboot/web/domain/user/User.java
@@ -0,0 +1,37 @@
+package com.jijang.book.springboot.web.domain.user;
+
+import com.jijang.book.springboot.web.domain.BaseTimeEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false)
+  private String email;
+
+  @Column
+  private String picture;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private Role role;
+
+
+}
